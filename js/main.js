8722947ebce0948acf2c0668d429946014824a42
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-21 14:27:27 +0800

    fix link to graudation ceremony

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+ * Version 
+ * Author: WildfootW
+ * GitHub: github.com/Wildfoot
+ * Copyright (C) 2018 WildfootW All rights reserved.
+ *
+ */
+
+$(document).ready(function(){
+    $(".link-to-graduation-ceremony").click(function(){
+        console.log("click");
+        window.location.href = "./Graduation_ceremony.html";
+    });
+});
